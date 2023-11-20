3bc7134cd332bfa87aed60d0d9cf17add808a802
Emiliano Bazan Zapata <emilianobz546@gmail.com>
2023-11-19 21:18:07 -0300

    Test Entity for Central Task Controller Without Documentation

---
+++ ApiRedoc/ApiRedoc/ViewModels/Request/TodoItemRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiRedoc.ViewModels.Request;
+
+public class TodoItemRequest
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool Completed { get; set; }
+}
--- ApiRedoc/ApiRedoc/ViewModelsWithRedoc/Request/TodoItemRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiRedoc.ViewModelsWithRedoc.Request;
-
-public class TodoItemRequest
-{
-    public long Id { get; set; }
-    public string Nombre { get; set; } = string.Empty;
-    public bool Completado { get; set; } = false;
-}
