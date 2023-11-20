4873559f59139c82adce2ae0f50fc646fcf519c8
Emiliano Bazan Zapata <emilianobz546@gmail.com>
2023-11-19 21:16:20 -0300

    It adds the two different ways to add documentation to the entity: TodoItemResponseDocumentation

---
+++ ApiRedoc/ApiRedoc/ViewModels/Response/TodoItemResponseDocumentation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ApiRedoc.ViewModels.Response;
+
+public class TodoItemResponseDocumentation
+{
+    [SwaggerSchema(Title = "Unique ID",
+        Description = "This is the database ID and will be unique.",
+        Format = "int")]
+    public int Id { get; set; }
+        
+    [Description("Name of the task to complete")]
+    public string Name { get; set; }
+
+    [Description("Indicates whether the task was completed or not")]
+    public bool Completed { get; set; }
+}
